9b9b26ab81be57e522b0a47e374a93fc0e88bce6
Kumaresan A <kumaresan@tartlabs.com>
2025-04-16 22:13:28 +0530

    Backend: Add data models and schemas

---
+++ backend/app/schemas/rating.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class RatingBase(BaseModel):
+    user_id: int
+    rating: float
+
+class RatingCreate(RatingBase):
+    pass
+
+class Rating(RatingBase):
+    id: int
+    movie_id: int
+
+    class Config:
+        from_attributes = True 
